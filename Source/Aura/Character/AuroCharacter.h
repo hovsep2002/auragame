67a5d4772589f70c7b08bbe10ae73433c30e9e8d
Hovsep2002 <hovsep@neographgames.com>
2024-01-10 18:54:45 +0400

    Main Character and Enemy character implementation

---
+++ Source/Aura/Character/AuroCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuroCharacterBase.h"
+#include "AuroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuroCharacter : public AAuroCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Character/AuroCharacterBase.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuroCharacterBase.generated.h"
+
+UCLASS()
+class AURA_API AAuroCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuroCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category="Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+};
